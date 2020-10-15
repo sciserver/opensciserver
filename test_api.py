f24c74203d7c5d81e8a9da152e34c41f1ecaf0e7
Arik W. Mitschang <arik@jhu.edu>
2020-10-15 14:15:59 -0400

    Adding tests for information leakage
    We do not want to expose emails for public info, and also some group
    info should be witheld. Adding check that the validate endpoint
    returns full set of info.

--- test_api.py
+++ test_api.py
@@ -1,13 +1,12 @@
 import unittest
 import requests
 import random
-from testutil import sciserverBase, tokenHeader, getUser, getEmail
+from testutil import sciserverBase, tokenHeader, getUser, getEmail, getToken
 
 class TestApi(unittest.TestCase):
 
     def test_fileservice_basic_volumes(self):
         r = requests.get(f'{sciserverBase()}/fileservice/api/volumes/', headers=tokenHeader())
-        print(r.json())
         storage = [i for i in r.json()['rootVolumes'] if i['name'] == 'Storage'][0]
         persistent = [i for i in storage['userVolumes'] if i['name'] == 'persistent'][0]
         temporary = [i for i in r.json()['rootVolumes'] if i['name'] == 'Temporary'][0]
@@ -32,7 +31,6 @@ class TestApi(unittest.TestCase):
         r = requests.get(f'{sciserverBase()}/racm/ugm/rest/user', headers=tokenHeader())
         assert(r.status_code == 200)
         assert(r.json()['username'] == getUser())
-        assert(r.json()['contactEmail'] == getEmail())
         assert(r.json()['visibility'] == 'PUBLIC')
 
     def test_racm_create_group(self):
@@ -54,3 +52,33 @@ class TestApi(unittest.TestCase):
         assert(len(members) == 1)
         userrole = [i['role'] for i in members if i['id'] == userid][0]
         assert(userrole == 'OWNER')
+
+    def test_racm_public_no_info_leak(self):
+        r = requests.get(f'{sciserverBase()}/racm/ugm/rest/users/public', headers=tokenHeader())
+        for group in r.json()['groups']:
+            assert('owner' not in group)
+            assert('description' not in group)
+            assert(group['memberUsers'] == [])
+            assert(group['memberGroups'] == [])
+
+    def test_login_portal_public_user_endpoints_no_info_leak(self):
+        r = requests.get(f'{sciserverBase()}/login-portal/api/users?name={getUser()}')
+        userinfo = r.json()[0]
+        if 'email' in userinfo:
+            assert(userinfo['email'] == '')
+        userid = userinfo['user_id']
+        r = requests.get(f'{sciserverBase()}/login-portal/api/users/{userid}')
+        userinfo = r.json()
+        if 'email' in userinfo:
+            assert(userinfo['email'] == '')
+        r = requests.get(f'{sciserverBase()}/login-portal/keystone/v3/users/{userid}')
+        userinfo = r.json()['user']
+        if 'email' in userinfo:
+            assert(userinfo['email'] == '')
+
+    def test_token_validation(self):
+        r = requests.get(f'{sciserverBase()}/login-portal/api/validate/{getToken()}')
+        assert(r.json()['email'] == getEmail())
+        assert(r.json()['username'] == getUser())
+        assert(type(r.json()['user_id']) == str)
+        assert(r.json()['user_id'] != '')
