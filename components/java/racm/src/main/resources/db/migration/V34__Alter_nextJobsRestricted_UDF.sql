879d96cc2158cc4d6a44faa4193665ebaeafc06e
GerardLemson <glemson1@jhu.edu>
2025-11-26 16:07:17 -0500

    formatting fixes

--- components/java/racm/src/main/resources/db/migration/V34__Alter_nextJobsRestricted_UDF.sql
+++ components/java/racm/src/main/resources/db/migration/V34__Alter_nextJobsRestricted_UDF.sql
@@ -1,9 +1,9 @@
 CREATE or ALTER function [racm].[nextJobsRestricted](@compmUUID varchar(64), @timeout real, 
-        @interval real, @maxNum integer,@maxPerUser integer) 
+        @interval real, @maxNum integer, @maxPerUser integer) 
 returns @rt table(
-	  jobId bigint,submitterId bigint,
+	  jobId bigint, submitterId bigint,
 	  computeDomainId bigint, submitTime datetime,
-	  usageWeight float,numQueued integer, numStarted integer, ranking integer) 
+	  usageWeight float, numQueued integer, numStarted integer, ranking integer) 
 as
 begin
 
@@ -14,8 +14,8 @@ select @computeDomainId = computeDomainId
  where uuid=@compmUUID
 ;
 
-declare @prev table (submitterId bigint, numJobs integer, numQueued integer
-   ,  numStarted integer, totTime float, usageWeight float)
+declare @prev table (submitterId bigint, numJobs integer, numQueued integer,
+                     numStarted integer, totTime float, usageWeight float)
 
 insert into @prev
 select dj.submitterId
@@ -35,26 +35,28 @@ select dj.submitterId
    and dj.startedTime >= dateadd(second,-@interval,getDate())
  group by submitterid
 
-declare @pending table (id bigint, submitterId bigint, computeDomainId bigint
-       , submitTime datetime,rankSubmitted integer)
+declare @pending table (id bigint, submitterId bigint, computeDomainId bigint,
+                        submitTime datetime,rankSubmitted integer)
 
 insert into @pending
-select dj.id, dj.submitterId, dj.computeDomainId, submitTime
-,      rank() over (partition by submitterId order by submitTime) as rankSubmitted
-  from compm c , job dj 
+select dj.id, dj.submitterId, dj.computeDomainId, submitTime,
+       rank() over (partition by submitterId order by submitTime) as rankSubmitted
+  from compm c
+  join job dj 
+    on dj.computeDomainId=c.computeDomainId
  where c.uuid = @compmUUID
-   and dj.computeDomainId=c.computeDomainId
    and dj.status=1 -- PENDING
 
 ;
 with final as (
-select j.Id as jobId,j.submitterId, j.computeDomainId, j.submitTime  -- without TOP cannot do an order by in a function
+select j.id as jobId,j.submitterId, j.computeDomainId, j.submitTime  
 ,      isnull(p.usageWeight,0) as usageWeight
 ,      isnull(p.numQueued,0) as numQueued
 ,      isnull(p.numStarted,0) as numStarted
 ,      rank() over (order by rankSubmitted,usageWeight,submitTime) as ranking
-  from @pending j left outer join @prev p
-  on p.submitterId = j.submitterid
+  from @pending j 
+  left outer join @prev p
+    on p.submitterId = j.submitterid
   where j.rankSubmitted <=@maxNum
     and j.rankSubmitted+isnull(p.numQueued,0)+isnull(p.numStarted,0)<=@maxPerUser
 )
@@ -62,7 +64,6 @@ insert into @rt
 select * -- jobId,submitterId, computeDomainId, submitTime , usageWeight, numQueued, numStarted, ranking
   from final
  where ranking <= @maxNum 
---   and rankSubmitted+numQueued+numStarted<=@maxPerUser -- to counter possibility COMPM gets single user too many times in internal queue
 
   return 
 end
